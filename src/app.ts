4a826851477ecfde8f67356490fd0ad2365ccb27
david1992121 <dantes0121@hotmail.com>
2022-10-03 22:31:07 +0800

    add stats data

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getFeeds } from './data';
+import { getFeeds, getStats } from './data';
 
 const app = express();
 const port = 3000;
@@ -9,26 +9,8 @@ app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
   const feedData = getFeeds();
-  const barData = {
-    "whales": {
-      "total": 1250,
-      "estProfit": 781.9,
-      "estProfitPercent": 54.05,
-      "estLoss": 781.9,
-      "estLossPercent": 42.16,
-      "gasFees": 781.9,
-      "gasFeesPercent": 7,
-      "bought": 781.9,
-      "boughtPercent": 66.76,
-      "sold": 781.9,
-      "soldPercent": 19.19,
-      "transferred": 781.9,
-      "transferredPercent": 2.7,
-      "listed": 781.9,
-      "listedPercent": 19.19
-    }
-  }
-  res.render('pages/index', {feedData, barData});
+  const barData = getStats();
+  res.render('pages/index', { feedData, barData });
 });
 
 app.listen(port, () => {
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,11 +1,14 @@
-import { readFileSync } from "fs";
-import path from "path";
+import { readFileSync } from 'fs';
+import path from 'path';
 
 const getData = (fileName: string) => {
-    return readFileSync(path.resolve(__dirname, fileName), 'utf8');
-}
+  return readFileSync(path.resolve(__dirname, fileName), 'utf8');
+};
 
-export const getFeeds = () => {    
-    const feedData = JSON.parse(getData("feeds.json"));
-    return feedData;
+export const getFeeds = () => {
+  return JSON.parse(getData('feeds.json'));
+};
+
+export const getStats = () => {
+  return JSON.parse(getData('stats.json'));
 }
